e4a5807a9c6668301792b9f7317eac84fc4c11a7
DevRobertoC <rc.rodriguez@live.com>
2022-11-30 21:17:35 -0500

    Avance proyecto backend

---
+++ app/models/order.model.js
@@ -0,0 +1,25 @@
+module.exports = mongoose => {
+    const Order = mongoose.model(
+      "order",
+      mongoose.Schema(
+        {
+          fecha: Date,
+          hora: String,
+          largo: String,
+          ancho: String,
+          alto: String,
+          peso: String,
+          direccionRecogida: String,
+          ciudadRecogida: String,
+          nombreDestinatario: String,
+          cedulaDestinatario: String,
+          direccionEntrega: String,
+          ciudadEntrega: String          
+        },
+        { timestamps: true }
+      )
+    );
+  
+    return Order;
+}; 
+
